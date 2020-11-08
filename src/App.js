f4f350ccd2fafa0733acda859183491ada0068fa
sajid507 <sajidmunawar666@gmail.com>
2020-11-08 10:20:42 +0500

    Added upload button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
 
+
 function App() {
-  return (
+  return (<div>
     <div >
       <Button variant="contained" color="primary">
       Hello World
     </Button><Button variant="contained" color="secondary" onClick={() => { alert('clicked') }}>Click me</Button>
+    </div>
+    <div>
+ 
+
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import  UploadButtons from "./App2"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <UploadButtons/>
   </React.StrictMode>,
   document.getElementById('root')
 );
